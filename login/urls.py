f39cdf57701b2c86ccf2e138f38d662e1243fcde
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-06-14 22:25:12 +0000

    add default login view

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-    path('register/', register)
+    path('register/', register),
+    path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
 ]
