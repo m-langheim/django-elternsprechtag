ec75ac68f26faabf1ed583f63c02024e15e28903
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-06-30 16:08:34 +0000

    leave codespaces

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', public_dashboard, name='home')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def public_dashboard(request):
+    return render(request, 'dashboard/public_dashboard.html')
