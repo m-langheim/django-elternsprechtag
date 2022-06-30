ab4840021361eaead295db270194adef3e5b3c5c
baastian <mattis.vanderbeek@protonmail.com>
2022-06-30 21:38:11 +0200

    registrationform activated

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,8 @@
+from cProfile import label
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django.utils.translation import gettext as _
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -19,3 +21,10 @@ class CustomUserChangeForm(UserChangeForm):
 
 class Register_OTP(forms.Form):
     otp = forms.IntegerField(label='One-Time-Password')
+
+
+class Register_Parent_Account(forms.Form):
+    email = forms.EmailField(label=_('Email'))
+    first_name = forms.CharField(label=_('FirstName'))
+    last_name = forms.CharField(label=_('Last Name'))
+    password = forms.CharField(widget=forms.PasswordInput)
