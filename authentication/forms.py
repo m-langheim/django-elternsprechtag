3f5fec29a14340ecd916af14acfd303c5811f064
baastian <mattis.vanderbeek@protonmail.com>
2022-07-02 12:59:39 +0200

    now the register page for parents is also designed

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from xml.dom import ValidationErr
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from .models import CustomUser
@@ -36,7 +37,20 @@ class Register_OTP(forms.Form):
 
 
 class Register_Parent_Account(forms.Form):
-    email = forms.EmailField(label=_('Email'))
-    first_name = forms.CharField(label=_('FirstName'))
-    last_name = forms.CharField(label=_('Last Name'))
-    password = forms.CharField(widget=forms.PasswordInput)
+    def validate_the_email(value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise forms.ValidationError(_('This email is already in use. Please select another email.'))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': _('Email'), 'autocomplete': 'off'}), validators=[validate_the_email], label=False)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': _('First Name'), 'autocomplete': 'off'}), label=False)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': _('Last Name'), 'autocomplete': 'off'}), label=False)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': _('Password'), 'autocomplete': 'off'}), max_length=255, label=False)
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': _('Confirm Password'), 'autocomplete': 'off'}), max_length=255, label=False)
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+
+        if password == confirm_password:
+            return self.cleaned_data
+        raise forms.ValidationError(_("a fuckin error."))
