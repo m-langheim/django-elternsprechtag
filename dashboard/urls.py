0f63a3103d7eaa378d30cff0b9415bdb71deb809
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-08-06 18:30:07 +0000

    add some translations

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', public_dashboard, name='home')
+    path('', public_dashboard, name='home'),
+    path('search/', public_dashboard, name='search')
 ]
