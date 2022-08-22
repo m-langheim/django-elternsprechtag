13536298a1c02b74c428a817358e359c3e6aeb24
baastian <mattis.vanderbeek@protonmail.com>
2022-08-22 09:52:39 +0200

    tags funktionieren aber halt nicht integriert

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from authentication.models import CustomUser
+from authentication.models import CustomUser, TeacherExtraData
 
 # Create your views here.
 
@@ -14,14 +14,19 @@ def public_dashboard(request):
 
 @login_required
 def search(request):
-    teacher = CustomUser.objects.filter(role=1)
+    teachers = CustomUser.objects.filter(role=1)
+    teacherExtraData = TeacherExtraData.objects.all()
     request_search = request.GET.get('q', None)
     if request_search is None:
         print('None')
     elif request_search.startswith('#'):
         request_search = request_search[1:]
-        result = teacher.filter(tags__icontains=request_search)
+        extraData = teacherExtraData.filter(tags__icontains=request_search)
+        result = []
+        for data in extraData:
+            teacher = data.teacher
+            result.append(teacher)
     else:
-        result = teacher.filter(last_name__icontains=request_search)
+        result = teachers.filter(last_name__icontains=request_search)
 
     return render(request, 'dashboard/search.html', {'teachers': result, 'search': request_search})
