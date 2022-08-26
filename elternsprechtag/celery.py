75cbe6893125018e1b1966a1b849cb1e5d908348
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-08-26 21:26:33 +0000

    celery correctly configured

--- elternsprechtag/celery.py
+++ elternsprechtag/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
-app = Celery("django_celery")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elternsprechtag.settings")
+app = Celery("elternsprechtag")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
