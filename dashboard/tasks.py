3d788f36c7bd43130ceac206e1be2fdb89a7d14c
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-08-31 10:02:23 +0000

    create event entrys with celery

---
+++ dashboard/tasks.py
@@ -0,0 +1,27 @@
+from time import sleep
+from dashboard.models import Event, TeacherStudentInquiry, SiteSettings
+import datetime
+from celery import shared_task
+
+from authentication.models import CustomUser
+
+
+@shared_task
+def async_create_events():
+    sleep(10)
+    teachers = CustomUser.objects.filter(role=1)
+
+    time_start = SiteSettings.objects.all().first().time_start
+    time_end = SiteSettings.objects.all().first().time_end
+    duration = SiteSettings.objects.all().first().event_duration
+    for teacher in teachers:
+
+        start = datetime.datetime.combine(
+            datetime.date.today(), time_start)
+        while start + duration <= datetime.datetime.combine(datetime.date.today(), time_end):
+            try:
+                Event.objects.get(teacher=teacher, start=start)
+            except Event.DoesNotExist:
+                Event.objects.create(
+                    teacher=teacher, start=start, end=start+duration)
+            start = start + duration
