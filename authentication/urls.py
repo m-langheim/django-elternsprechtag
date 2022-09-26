b61d14ebbe1cb44e61f61ba558b4ca76a7a4a361
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-09-26 21:45:55 +0000

    add password reset

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,31 @@ from .views import *
 
 urlpatterns = [
     path('register/<user_token>/<key_token>/', register),
-    path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html', authentication_form=CustomAuthForm), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html',
+         authentication_form=CustomAuthForm, redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
-        template_name='authentication/logout.html'), name='logout')
+        template_name='authentication/logout.html'), name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='authentication/password-reset/password_reset.html',
+             subject_template_name='authentication/password-reset/password_reset_subject.txt',
+             email_template_name='authentication/password-reset/password_reset_email.html',
+             success_url='/login/'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='authentication/password-reset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='authentication/password-reset/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='authentication/password-reset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
