17eac9cbf225cb792b08d57d850c12bc3fb99306
Jan-Niklas Wielpütz <janni.wielpuetz@gmail.com>
2023-03-09 16:06:15 +0100

    change reset email

--- elternsprechtag/celery.py
+++ elternsprechtag/celery.py
@@ -1,7 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elternsprechtag.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "elternsprechtag.settings.production")  # ! Always needs to be changed befor build
+
 app = Celery("elternsprechtag")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
