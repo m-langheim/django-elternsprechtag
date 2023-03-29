47fb4daace0350d66ff5e94e7b12c343ca37778e
baastian <mattis.vanderbeek@protonmail.com>
2023-03-29 16:52:50 +0200

    navbar little fix, added comments in views in auth added to the register_choose that you will logout by clicking on "login" after that you will be redirected to login again

--- authentication/views.py
+++ authentication/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render, redirect
 from .models import Upcomming_User, CustomUser
 from django.db.models import Q
 from .forms import *
+from .tasks import async_send_mail
 from django.utils import timezone
 from django.contrib import messages
 from django.utils.translation import gettext as _
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
-from django.contrib.auth.forms import PasswordResetForm
-from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 import os
-from .tasks import async_send_mail
 
 
 def register(request, user_token, key_token):
+
+    # First checks and initialization
+
     if user_token is None or key_token is None:
         return redirect("help_register")
 
@@ -33,30 +33,45 @@ def register(request, user_token, key_token):
     user_data = user_data.first()
 
     if user_data.created + timezone.timedelta(days=30) < timezone.now():
-        studi = user_data.student
+        student = user_data.student
         user_data.delete()
-        Upcomming_User.objects.create(student=studi)
+        Upcomming_User.objects.create(student=student)
         return render(request, 'authentication/register/link_deprecated.html')
 
-    if user_data.otp_verified:
-        # check if otp was set to verified in last 3 hours
-        if user_data.otp_verified_date + timezone.timedelta(hours=3) > timezone.now():
-            if request.GET.get('login', False) and request.user.is_authenticated:
-                if request.user.role == 0:
+    # Show different pages
+
+    if user_data.otp_verified: # OTP verified
+        
+        if user_data.otp_verified_date + timezone.timedelta(hours=3) > timezone.now(): # OTP not verified in the last 3 hours
+
+            # Page where you can choose to login or register
+
+            if request.GET.get('login', False) and request.user.is_authenticated: # Clicked on 'login' and logged into an account again
+
+                # Which type of account
+
+                if request.user.role == 0: # Logged into a parent account -> Add the student to this account
                     user = request.user
                     user.students.add(user_data.student)
                     user.save()
+                    name = user_data.student
                     user_data.delete()
 
-                    return redirect("home")
-                else:
-                    logout(request)
-                    return render(request, 'authentication/register/unusable_account.html')
+                    # TODO: Email, dass Schüler zum Account hinzugefügt wurde
 
-            if request.GET.get('register', False):
-                if request.method == 'POST':
+                    messages.success(request, f"You added {name} to your parent account.") # TODO: Hier kann man auch die message durch eine Mitteilung ersetzen.
+                    return redirect("home")
+                else: # Logged into teacher or staff account -> Logout and re-login
+                    logout(request)
+                    return render(request, 'authentication/register/unusable_account.html', {'path': request.get_full_path()})
+
+            if request.GET.get('register', False): # Clicked on 'register'
+
+                # Clicked on 'submit' or load this page
+                
+                if request.method == 'POST': # Clicked on 'submit'
                     form = Register_Parent_Account(request.POST)
-                    if form.is_valid():  # Erstellung des Nutzers
+                    if form.is_valid():
                         cu = CustomUser(
                             email=form.cleaned_data['email'],
                             first_name=form.cleaned_data['first_name'],
@@ -66,76 +81,77 @@ def register(request, user_token, key_token):
                         cu.students.add(user_data.student)
                         cu.save()
                         user_data.delete()
-                        async_send_mail.delay("Registrierung erfolgreich", render_to_string(
-                            "authentication/register/register_finished_email.txt", {'user': cu, 'current_site': os.environ.get("PUBLIC_URL")}), cu.email)
-                        # {'page': request.GET.get("page")}
-                        # logout user
+
+                        # TODO: Hier muss hin, dass der Schüler zum Account hinzugefügt wurd
+
+                        # Send confirmation mail
+                        async_send_mail.delay("Registrierung erfolgreich", render_to_string("authentication/register/register_finished_email.txt", {'user': cu, 'current_site': os.environ.get("PUBLIC_URL")}), cu.email)
+
                         if request.user.is_authenticated:
                             logout(request)
-                            messages.info(
-                                request, "Sie wurden abgemeldet um den Registrierungsvorgang fortzusetzen.")
+                            messages.info(request, "You are logged out to continue the registration process.")
+
                         return redirect('login')
 
                 else:
                     form = Register_Parent_Account()
+                
+                return render(request, "authentication/register/register_parent.html", {'register_parent_account': form})
+            
+            # Set name for the register-choose page
 
-                return render(
-                    request,
-                    "authentication/register/register_parent.html",
-                    {'register_parent_account': form})
-            name = user_data.student
-            name = str(name)
+            name = str(user_data.student)
             if len(name) > 20:
                 name = name[:18]
                 name = name+'...'
-            # view to choose between registering a new user and logging in
-            return render(
-                request,
-                "authentication/register/register_choose.html",
-                {'child_name': name, 'path': request.get_full_path()})
-        else:  # otp was verified more than 3 hours ago
-            # messages.warning(
-            #     request, _("The validation has timed out, please reenter your pin"))
+
+            return render(request, "authentication/register/register_choose.html", {'child_name': name, 'path': request.get_full_path()})
+        
+        else:  # OTP verified more than 3 hours ago
             user_data.otp_verified = False
             user_data.save()
             form = Register_OTP(user_token=user_token, key_token=key_token)
-            name = user_data.student
-            name = str(name)
+
+            # Set name for the register-otp page
+
+            name = str(user_data.student)
             if len(name) > 20:
                 name = name[:18]
                 name = name+'...'
-            return render(
-                request,
-                'authentication/register/register_otp.html',
-                {'otp_form': form, 'child_name': name})
-    else:
-        if request.method == 'POST':
+
+            return render(request, 'authentication/register/register_otp.html', {'otp_form': form, 'child_name': name})
+        
+    else: # OTP not verified
+
+        # Clicked on 'submit' or load this page
+
+        if request.method == 'POST': # Clicked on 'submit'
             form = Register_OTP(request.POST, user_token=user_token, key_token=key_token)
             if form.is_valid():
                 user_data.otp_verified = True
                 user_data.otp_verified_date = timezone.now()
                 user_data.save()
-                name = user_data.student
-                name = str(name)
+
+                # Set name for the register-choose page
+
+                name = str(user_data.student)
                 if len(name) > 20:
                     name = name[:18]
                     name = name+'...'
-                return render(
-                    request,
-                    "authentication/register/register_choose.html",
-                    {'child_name': name, 'path': request.get_full_path()})
+
+                return render(request, "authentication/register/register_choose.html", {'child_name': name, 'path': request.get_full_path()})
 
         else:
             form = Register_OTP(user_token=user_token, key_token=key_token)
-        name = user_data.student
-        name = str(name)
+
+        # Set name for the register-otp page
+
+        name = str(user_data.student)
         if len(name) > 20:
             name = name[:18]
             name = name+'...'
-        return render(
-            request,
-            'authentication/register/register_otp.html',
-            {'otp_form': form, 'child_name': name})
+
+        return render(request, 'authentication/register/register_otp.html', {'otp_form': form, 'child_name': name})
 
 
 def password_reset_request(request):
