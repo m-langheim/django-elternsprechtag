4720edac001e2c445680cb0ed067e40ff7bf740d
janni06 <janni.wielpuetz@gmail.com>
2023-09-20 23:50:45 +0200

    don´t start celery

--- elternsprechtag/celery.py
+++ elternsprechtag/celery.py
@@ -1,18 +1,18 @@
-# import os
-# from celery import Celery
-# from celery.schedules import crontab
+import os
+from celery import Celery
+from celery.schedules import crontab
 
-# # from general_tasks.tasks import run_dbbackup
+# from general_tasks.tasks import run_dbbackup
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-#                       "elternsprechtag.settings.development")  # ! Always needs to be changed befor build
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "elternsprechtag.settings.development")  # ! Always needs to be changed befor build
 
-# app = Celery("elternsprechtag")
-# app.config_from_object("django.conf:settings", namespace="CELERY")
+app = Celery("elternsprechtag")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# app.autodiscover_tasks()
+app.autodiscover_tasks()
 
 
-# # @app.on_after_configure.connect
-# # def register_periodic_tasks(sender: Celery, **kwargs):
-# #     sender.add_periodic_task(10.0, run_dbbackup.s())
+# @app.on_after_configure.connect
+# def register_periodic_tasks(sender: Celery, **kwargs):
+#     sender.add_periodic_task(10.0, run_dbbackup.s())
