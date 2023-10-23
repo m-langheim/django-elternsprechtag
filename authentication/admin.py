cf86d9f3f43bf0ed26ce2dbc9039bb9236eac987
janni06 <janni.wielpuetz@gmail.com>
2023-10-23 16:53:37 +0200

    auth admin search and sort

--- authentication/admin.py
+++ authentication/admin.py
@@ -23,25 +23,65 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_active', 'role')
-    list_filter = ('is_active', 'role', 'is_staff')
+    list_display = ("email", "is_active", "role")
+    list_filter = ("is_active", "role", "is_staff")
     fieldsets = (
-        (None, {'fields': ('email', 'password',
-         'first_name', 'last_name', 'role', 'students')}),
-        (_('Permissions'), {'fields': ('is_staff', 'is_active')}),
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                    "first_name",
+                    "last_name",
+                    "role",
+                    "students",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_staff",
+                    "is_admin",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ["collapse"],
+            },
+        ),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'role')}
-         ),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "is_staff",
+                    "role",
+                ),
+            },
+        ),
     )
-    search_fields = ('email', 'first_name', 'last_name',)
-    ordering = ('email',)
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+    ordering = ("email",)
 
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'color')
+    list_display = ("name", "color")
+    ordering = ("name",)
+    search_fields = ("name", "synonyms")
 
 
 class UpcommingsUserAdmin(admin.ModelAdmin):
@@ -52,12 +92,30 @@ class UpcommingsUserAdmin(admin.ModelAdmin):
             current_site = os.environ.get("PUBLIC_URL")
             email_subject = "Anmeldelink für den Elternsprechtag"
             email_str_body = render_to_string(
-                'authentication/email/link.html', {'current_site': current_site, 'id': up_user.user_token, 'key': up_user.access_key, 'otp': up_user.otp})
+                "authentication/email/link.html",
+                {
+                    "current_site": current_site,
+                    "id": up_user.user_token,
+                    "key": up_user.access_key,
+                    "otp": up_user.otp,
+                },
+            )
             email_html_body = render_to_string(
-                'authentication/email/link_html.html', {'current_site': current_site, 'id': up_user.user_token, 'key': up_user.access_key, 'otp': up_user.otp})
+                "authentication/email/link_html.html",
+                {
+                    "current_site": current_site,
+                    "id": up_user.user_token,
+                    "key": up_user.access_key,
+                    "otp": up_user.otp,
+                },
+            )
 
-            async_send_mail.delay(email_subject, email_str_body,
-                                  up_user.student.child_email, email_html_body=email_html_body)
+            async_send_mail.delay(
+                email_subject,
+                email_str_body,
+                up_user.student.child_email,
+                email_html_body=email_html_body,
+            )
             up_user.email_send = True
             up_user.save()
             successfull_updates += 1
@@ -83,12 +141,30 @@ class UpcommingsUserAdmin(admin.ModelAdmin):
             current_site = os.environ.get("PUBLIC_URL")
             email_subject = "Anmeldelink für den Elternsprechtag"
             email_str_body = render_to_string(
-                'authentication/email/link.html', {'current_site': current_site, 'id': new_up_user.user_token, 'key': new_up_user.access_key, 'otp': new_up_user.otp})
+                "authentication/email/link.html",
+                {
+                    "current_site": current_site,
+                    "id": new_up_user.user_token,
+                    "key": new_up_user.access_key,
+                    "otp": new_up_user.otp,
+                },
+            )
             email_html_body = render_to_string(
-                'authentication/email/link_html.html', {'current_site': current_site, 'id': new_up_user.user_token, 'key': new_up_user.access_key, 'otp': new_up_user.otp})
+                "authentication/email/link_html.html",
+                {
+                    "current_site": current_site,
+                    "id": new_up_user.user_token,
+                    "key": new_up_user.access_key,
+                    "otp": new_up_user.otp,
+                },
+            )
 
-            async_send_mail.delay(email_subject, email_str_body,
-                                  new_up_user.student.child_email, email_html_body=email_html_body)
+            async_send_mail.delay(
+                email_subject,
+                email_str_body,
+                new_up_user.student.child_email,
+                email_html_body=email_html_body,
+            )
             new_up_user.email_send = True
             new_up_user.save()
             successfull_updates += 1
@@ -108,9 +184,19 @@ class UpcommingsUserAdmin(admin.ModelAdmin):
     search_fields = ["student"]
     actions = [sendRegistrationMails, recreateUpcommingUser]
     fieldsets = (
-        (None, {'fields': ('student', 'email_send', 'created')}),
-        (_('Access details'), {'fields': (
-            'user_token', 'access_key', 'otp', 'otp_verified', 'otp_verified_date')}),
+        (None, {"fields": ("student", "email_send", "created")}),
+        (
+            _("Access details"),
+            {
+                "fields": (
+                    "user_token",
+                    "access_key",
+                    "otp",
+                    "otp_verified",
+                    "otp_verified_date",
+                )
+            },
+        ),
     )
 
 
@@ -126,10 +212,14 @@ admin.site.register(TeacherExtraData)
 class StudentAdmin(admin.ModelAdmin):
     change_list_template = "authentication/admin/students_changelist.html"
 
+    list_display = ("last_name", "first_name", "registered")
+
+    search_fields = ["first_name", "last_name"]
+
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
-            path('import-csv/', self.import_csv),
+            path("import-csv/", self.import_csv),
         ]
         return my_urls + urls
 
@@ -139,16 +229,22 @@ class StudentAdmin(admin.ModelAdmin):
             for group in Group.objects.filter(name__startswith="class_"):
                 group.delete()
 
-            csv_file = request.FILES["csv_file"].read().decode('utf-8-sig')
-            reader = csv.DictReader(io.StringIO(csv_file), delimiter=';')
+            csv_file = request.FILES["csv_file"].read().decode("utf-8-sig")
+            reader = csv.DictReader(io.StringIO(csv_file), delimiter=";")
 
             created_students = 0
             for lines in reader:
                 student = Student.objects.filter(
-                    shield_id=lines["eindeutige Nummer (GUID)"])
+                    shield_id=lines["eindeutige Nummer (GUID)"]
+                )
                 if not student.exists():
                     student = Student.objects.create(
-                        shield_id=lines["eindeutige Nummer (GUID)"], first_name=lines["Vorname"], last_name=lines["Nachname"], class_name=lines["Klasse"], child_email=lines["Mailadresse"])
+                        shield_id=lines["eindeutige Nummer (GUID)"],
+                        first_name=lines["Vorname"],
+                        last_name=lines["Nachname"],
+                        class_name=lines["Klasse"],
+                        child_email=lines["Mailadresse"],
+                    )
                 else:
                     student = student.first()
                     student.child_email = lines["Mailadresse"]
@@ -170,6 +266,4 @@ class StudentAdmin(admin.ModelAdmin):
             return redirect("..")
         form = AdminCsvImportForm()
         payload = {"form": form}
-        return render(
-            request, "authentication/admin/csv_form.html", payload
-        )
+        return render(request, "authentication/admin/csv_form.html", payload)
