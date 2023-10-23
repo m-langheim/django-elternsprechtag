8b11386796a68ebdbe64eee570b64570689492b1
janni06 <janni.wielpuetz@gmail.com>
2023-10-23 15:20:11 +0200

    log path

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,10 +4,10 @@ bind = "0.0.0.0:8000"
 workers = 3
 # Access log - records incoming HTTP requests
 # accesslog = "/var/log/gunicorn.access.log"
-accesslog = "log/gunicorn/gunicorn-access.log"
+accesslog = "./log/gunicorn/gunicorn-access.log"
 # Error log - records Gunicorn server goings-on
 # errorlog = "/var/log/gunicorn.error.log"
-errorlog = "log/gunicorn/gunicorn-error.log"
+errorlog = "./log/gunicorn/gunicorn-error.log"
 # Whether to send Django output to the error log
 capture_output = True
 # How verbose the Gunicorn error logs should be
