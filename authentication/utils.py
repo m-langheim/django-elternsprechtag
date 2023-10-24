0f7bfb47f187321bed20738aff48b15f56d3205e
janni06 <janni.wielpuetz@gmail.com>
2023-10-24 09:44:45 +0200

    bug fix teacher register

--- authentication/utils.py
+++ authentication/utils.py
@@ -13,8 +13,19 @@ from .tasks import async_send_mail
 
 
 def register_new_teacher(email: str):
-    if not CustomUser.objects.filter(Q(email=email), Q(role=1), Q(is_active=True)):
-        new_teacher = CustomUser(role=1, is_staff=True, is_active=False, email=email)
+    if not CustomUser.objects.filter(
+        Q(email=email), Q(role=1), Q(is_active=True)
+    ).exists():
+        if not CustomUser.objects.filter(email=email).exists():
+            new_teacher = CustomUser(
+                role=1, is_staff=True, is_active=False, email=email
+            )
+        elif not CustomUser.objects.filter(Q(email=email), Q(role=1)).exists():
+            raise "Dieser Email wird bereits von einem nicht Lehrer genutzt."
+        elif CustomUser.objects.filter(Q(email=email), Q(role=1), Q(is_active=False)):
+            new_teacher = CustomUser.objects.get(email=email)
+        else:
+            raise "Es ist ein Fehler bei der Erstellung des Lehrer Accounts aufgetretetn."
         new_teacher.set_unusable_password()  # Hier wird ein nicht benutzbares Passwort festgelegt
         new_teacher.save()
 
