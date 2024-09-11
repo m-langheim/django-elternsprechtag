7edc154cf6e7559feb68fc7a2728bdacb3e8865d
baastian <mattis.vanderbeek@protonmail.com>
2024-09-12 00:04:52 +0200

    laid the foundation for implementation of help pages via block help_url_smallscreen and block help_url_bigscreen you enter the help url for the specific page + the redirect in urls.py to the wiki page

--- help/urls.py
+++ help/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # Der hier muss noch komplett raus (wir machen glaube ich keine Help mehr über eigene Seiten):
     path('register/', register_help, name="help_register"),
 
-    path('redirect/wiki', RedirectView.as_view(url='https://wiki.jhg-elternsprechtag.de/'), name='wiki_mainpage')
+    path('redirect/wiki', RedirectView.as_view(url='https://wiki.jhg-elternsprechtag.de/'), name='wiki_mainpage'),
+    path('redirect/wiki/public_dashboard', RedirectView.as_view(url='https://wiki.jhg-elternsprechtag.de/books/startseite'), name='wiki_public_dashboard')
 ]
