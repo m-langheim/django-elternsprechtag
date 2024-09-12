98219d42932f765d46345c01f9918d40f6ca7fe4
baastian <mattis.vanderbeek@protonmail.com>
2024-09-12 16:33:31 +0200

    removed unused help pages

--- help/urls.py
+++ help/urls.py
@@ -3,9 +3,6 @@ from django.views.generic.base import RedirectView
 from .views import register_help
 
 urlpatterns = [
-    # Der hier muss noch komplett raus (wir machen glaube ich keine Help mehr über eigene Seiten):
-    path('register/', register_help, name="help_register"),
-
     path('redirect/wiki', RedirectView.as_view(url='https://wiki.jhg-elternsprechtag.de/'), name='wiki_mainpage'),
     path('redirect/wiki/public_dashboard', RedirectView.as_view(url='https://wiki.jhg-elternsprechtag.de/books/startseite'), name='wiki_public_dashboard'),
     path('redirect/wiki/parent_search', RedirectView.as_view(url='https://wiki.jhg-elternsprechtag.de/'), name='wiki_parent_search'),
--- help/views.py
+++ help/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def register_help(request):
-    return render(request, 'help/register_help/register_help.html', {'name':request.GET.get('u')})
