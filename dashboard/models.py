1ed9b9348a9816980a9584e1af534b162980eb63
janni06 <janni.wielpuetz@gmail.com>
2024-10-11 12:58:14 +0200

    add id to formular

--- dashboard/models.py
+++ dashboard/models.py
@@ -437,7 +437,7 @@ class EventChangeFormula(models.Model):
     Dieses Model dient dazu, jedem Lehrer die Möglichkeit zu geben, seine Zeiten für den Elternsprtechtag selber einzurrichten. In Zukunft können hier auch Anträge auf die Blockierung einzelner Termine eingereicht werden.
     """
 
-    # id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
 
     class FormularTypeChoices(models.IntegerChoices):
         TIME_PERIODS = 0, _("Time period")
