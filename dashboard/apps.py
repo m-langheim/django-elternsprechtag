44f9d7ff6a2d72052f7a4329f3df91e8199ef5dd
janni06 <janni.wielpuetz@gmail.com>
2024-10-11 08:37:02 +0200

    Sitesetting uncommented

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,11 +2,11 @@ from django.apps import AppConfig
 
 
 class DashboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dashboard'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dashboard"
 
     def ready(self):
 
-        from .models import SiteSettings
-        # SiteSettings.objects.create()
+        #     from .models import SiteSettings
+        #     # SiteSettings.objects.create()
         import dashboard.signals
